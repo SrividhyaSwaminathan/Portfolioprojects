8b015a776792e652726df4dd98f587d8a8ebb8a7
SrividhyaSwaminathan <123998916+SrividhyaSwaminathan@users.noreply.github.com>
2023-03-22 18:08:56 -0400

    Update CovidPortfolioprojects.sql

--- CovidPortfolioprojects.sql
+++ CovidPortfolioprojects.sql
@@ -1,3 +1,10 @@
+/*
+Covid19 Data Exploration
+Skills :JOINS,Aggregate Functions,Creating Views,Temp Tables,Data Type Conversion,CTE
+*/
+
+
+
 select * from
 PortfolioProject..CovidDeaths
 order by 3,4
@@ -6,22 +13,31 @@ order by 3,4
 --PortfolioProject..CovidVaccinations
 ----order by 3,4
 
+-- Select the Data that we are going to start with
 select location,date,total_cases,new_cases,total_deaths,population
 from
 PortfolioProject..CovidDeaths
 
+
+--Total Death Percentage
 select location,date,Population,total_deaths,(total_deaths/population)*100 AS Death_percentage
 from
 PortfolioProject..CovidDeaths
 ----where location like '%states%'
 order by 1,2
 
+
+--Total Cases Vs Total Population
+--Shows what percentage of people are infected with COVID
 select location,Population,MAX(total_cases) AS HighestInfectionCount,MAX((total_cases/population))*100 AS PercentPopulationInfected
 from
 PortfolioProject..CovidDeaths
 GROUP BY population,location
 order by PercentPopulationInfected desc
 
+
+
+--Countries with Highest Death count per population
 select location,MAX(cast(total_deaths as int)) AS Total_DeathCount
 from
 PortfolioProject..CovidDeaths
@@ -29,7 +45,7 @@ where continent is NOT NULL
 GROUP BY location
 order by Total_DeathCount DESC
 
-
+-- Showing contintents with the highest Death count per population
 select continent,MAX(cast(total_deaths as int)) AS Total_DeathCount
 from
 PortfolioProject..CovidDeaths
@@ -37,13 +53,17 @@ where continent is NOT NULL
 GROUP BY continent
 order by Total_DeathCount DESC
 
-
+--Global numbers
 select SUM(new_cases) AS TotalCases,sum(cast(new_deaths as int)) AS TotalDeaths,sum(cast(new_deaths as int))/SUM(new_cases)*100 as Death_Percentage
 from
 PortfolioProject..CovidDeaths
 where continent is not null
 --group by date
 order by 1,2
+
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 select dea.continent,dea.location,dea.date,dea.population,vac.new_vaccinations,SUM(cast(vac.new_vaccinations as float)) OVER (partition by dea.location ORDER BY dea.location,dea.date) as RollingPeopleVaccinated,(RollingPeopleVaccinated/population)*100
 from PortfolioProject..CovidDeaths  Dea JOIN 
@@ -53,6 +73,7 @@ Dea.date=Vac.date
 where dea.continent is not null
 ORDER BY 2,3
 
+-- Using CTE to perform Calculation on Partition By in previous query
 
 	WITH PopvsVacc (Continent,location,date,population,new_vaccinations,RollingPeopleVaccinated)
 	as
@@ -68,6 +89,9 @@ ORDER BY 2,3
 	--ORDER BY 2,3
 	)
 	select *,(RollingPeopleVaccinated/population)*100 from PopvsVacc
+
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
 
 
 	DROP table if exists  #PercentPopulationVaccinated
@@ -90,6 +114,10 @@ create table #PercentPopulationVaccinated
 	--where dea.continent is not null
 	--ORDER BY 2,3
 	select *,(RollingPeopleVaccinated/population)*100 from #PercentPopulationVaccinated
+	
+	
+	
+	-- Creating View to store data for later visualizations
 
 
 	Create View PercentPopulationVaccinated as
